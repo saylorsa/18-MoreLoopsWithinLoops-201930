068738f33b87097dfa2550304ca35423dfcbb88c
saylorsa <saylorsa@rose-hulman.edu>
2019-04-24 21:48:35 -0400

    m1, Done - 7

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -346,7 +346,7 @@ def numbers_increasing_forward(r, maxnum):
     Preconditions:  r and maxnum are positive integers.
     """
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -354,6 +354,12 @@ def numbers_increasing_forward(r, maxnum):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
+    for i in range(r):
+        for j in range(maxnum):
+            for k in range(j+1):
+                print(j+1, end='')
+            print(' ', end='')
+        print()
 
 
 # -----------------------------------------------------------------------------
